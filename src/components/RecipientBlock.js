8e500e7d6d75ae82ee7f34b908a6195fcf414a1c
s9379992g <s9379992g@gmail.com>
2021-12-24 18:03:37 +0200

    added recipient-info__btn

--- src/components/RecipientBlock.js
+++ src/components/RecipientBlock.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import img from '../assets/images/user_heart_both.svg';
+import img2 from '../assets/images/girl.png';
 
 const RecipientBlock = () => {
   return (
-    <div className='recipent'>
-      <div className='recipent-avatar'>
-        <img
-          className='recipent-img'
-          src='https://ice.dating/files/girl_1.png'
-          alt='img'
-        />
+    <div className='recipient'>
+      <div className='recipient-avatar'>
+        <img className='recipient-img' src={img2} alt='img' />
       </div>
-      <div className='recipent-info'>
-        <div className='recipient-info__name verified'>Сара, 24</div>
-        <div className='recipent-info__status'>
-          <img src={img} alt='ico' />
-          <p>Вы понравились друг другу вчера</p>
+      <div className='recipient-info'>
+        <div className='recipient-info__wrap'>
+          <div className='recipient-info__name verified'>Сара, 24</div>
+          <div className='recipient-info__status'>
+            <img src={img} alt='ico' />
+            <p>Вы понравились друг другу вчера</p>
+          </div>
         </div>
+        <button className='recipient-info__btn' />
       </div>
     </div>
   );
